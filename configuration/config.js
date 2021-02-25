0bf5137756d6271e9f9900cc0a95c0f41cb4e9d9
Andressa Schinoff <51170291+andressaschinoff@users.noreply.github.com>
2021-02-25 09:36:55 -0300

    Update config.js

--- configuration/config.js
+++ configuration/config.js
@@ -2,7 +2,7 @@ module.exports={
     "api_key": "yourKey.apps.googleusercontent.com",
     "api_secret": "secretKey",
     "callback_url": "http://localhost:3000/auth/google/callback",
-    "api_key_face": "464357224929533",
-    "api_secret_face": "4281aa49e361f808ac480daddd9bd4ec",
+    "api_key_face": "",
+    "api_secret_face": "",
     "callback_url_face": "http://localhost:3000/auth/facebook/callback"
 }
