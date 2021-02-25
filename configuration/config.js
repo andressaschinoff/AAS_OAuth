edbfb7e068318728a1ee35080d2ccc2b0665facd
andressa.alves <andressa.alves@syonet.com>
2021-02-24 21:30:15 -0300

    OAuth passport facebook

---
+++ configuration/config.js
@@ -0,0 +1,8 @@
+module.exports={
+    "api_key": "yourKey.apps.googleusercontent.com",
+    "api_secret": "secretKey",
+    "callback_url": "http://localhost:3000/auth/google/callback",
+    "api_key_face": "464357224929533",
+    "api_secret_face": "4281aa49e361f808ac480daddd9bd4ec",
+    "callback_url_face": "http://localhost:3000/auth/facebook/callback"
+}
